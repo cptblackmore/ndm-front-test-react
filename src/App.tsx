96a82b41c0f98e70c8e462fc22896c00023e1b6a
cptblackmore <redshatterr@gmail.com>
2025-06-12 19:44:24 +0300

    feat(app): wrap in ThemeProvider, apply GlobalStyles and mount RoutesPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { ThemeProvider } from "@emotion/react"
+import { theme } from "./styles/theme"
+import { GlobalStyles } from "./styles/GlobalStyles"
+import { RoutesPage } from "./pages/RoutesPage"
+
 function App() {
   return (
     <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <RoutesPage />
+      </ThemeProvider>
     </>
   )
 }
